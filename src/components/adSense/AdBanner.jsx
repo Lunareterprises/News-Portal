7985bee30824b0cfed4632708dcb7693d153505e
Lunareterprises <lunarenterprisestvm@gmail.com>
2025-05-19 11:42:47 +0530

    addsecense added

---
+++ src/components/adSense/AdBanner.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+export default function AdBanner() {
+  useEffect(() => {
+    try {
+      if (typeof window !== "undefined") {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (e) {
+      console.error("Adsense error:", e);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle" // ✅ FIXED
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-5296998056401590"
+      data-ad-slot="2226315840"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+}
